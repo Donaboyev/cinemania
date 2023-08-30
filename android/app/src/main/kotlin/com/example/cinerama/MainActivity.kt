0f444edd5ce92d707d37120cd46cc5b390667713
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2023-08-30 18:04:44 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cinerama/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cinerama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:cinerama/core/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('${AppConstants.apiKey}'),
+      ),
+    );
+  }
+}
